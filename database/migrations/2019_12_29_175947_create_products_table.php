57c2d9c91ca13b56e78629d1750a2afba7ad71e6
vagrant <vagrant@homestead>
2019-12-29 18:26:56 +0000

    Add product migration, controller and model

---
+++ database/migrations/2019_12_29_175947_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku');
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug');
+            $table->float('storage')->default(0);
+            $table->double('price')->default(0);
+            $table->double('special_price')->nullable();
+            $table->timestamp('special_price_from')->nullable();
+            $table->timestamp('special_price_to')->nullable();
+            $table->boolean('visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
