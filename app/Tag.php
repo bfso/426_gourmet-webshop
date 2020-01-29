e954b11e0c8e9b3a3ad9b9b7d1e75d22f60f46b7
christoph.karlen@gmail.com <christoph.karlen@gmail.com>
2020-01-29 16:09:03 +0100

    Remove default migrations since the db is very old

--- app/Tag.php
+++ app/Tag.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    //
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
